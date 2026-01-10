7fabc37c58a8c0e04f502ccd96d1b449611d50e5
Dhesheswar <dhesheswark@gmail.com>
2026-01-10 18:20:03 +0530

    second largest

---
+++ src/ProbOnArrays/SecondLargest.java
@@ -0,0 +1,24 @@
+package ProbOnArrays;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class SecondLargest {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("src/input.txt"));
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        int largest = 0, secondLargest = -1;
+        for(int i=0;i<n;i++){
+            if (arr[i] > largest){
+                secondLargest = largest;
+                largest = arr[i];
+            }
+        }
+        System.out.println("Largest : "+largest+" | "+"Second Largest : "+secondLargest);
+    }
+}
