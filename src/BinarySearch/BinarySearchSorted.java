25e95892151ae042a5b7aa661052a497f369c290
Dhesheswar <dhesheswark@gmail.com>
2026-04-04 22:26:38 +0530

    changes file name

--- src/BinarySearch/BinarySearchSorted.java
+++ src/BinarySearch/BinarySearchSorted.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class binarySearchSorted {
+public class BinarySearchSorted {
     public static void main(String[] args) throws FileNotFoundException {
         Scanner sc = new Scanner(new File("src/input.txt"));
         int n = sc.nextInt();
