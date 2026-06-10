7728419502818a0623fda3eab9e73ac9a718419c
Dhesheswar <dhesheswark@gmail.com>
2026-06-11 00:15:19 +0530

    added largest odd number

---
+++ src/Strings/LargestOddNumber.java
@@ -0,0 +1,27 @@
+package Strings;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class LargestOddNumber {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("src/input.txt"));
+        String s = sc.nextLine();
+        //String str = String.valueOf(Integer.parseInt(s));
+        int j = 0;
+        while(j<s.length()-1 && s.charAt(j) == '0'){
+            j++;
+        }
+        String str = s.substring(j,s.length());
+        String result="";
+        for(int i = str.length()-1;i>=0;i--){
+            if(((str.charAt(i)-'0') & 1) ==1){
+                result = str.substring(0,i+1);
+                break;
+            }
+        }
+        System.out.println(result);
+    }
+}
+
--- src/Strings/RemoveOutermostParenthesis.java
+++ src/Strings/RemoveOutermostParenthesis.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class removeOutermostParenthesis {
+public class RemoveOutermostParenthesis {
     public static void main(String[] args) throws FileNotFoundException {
         Scanner sc = new Scanner(new File("src/input.txt"));
         String s = sc.nextLine();
