2bfacf164fd10b8183b8d62c16b74a25715a0e07
Dhesheswar <dhesheswark@gmail.com>
2026-06-10 23:34:47 +0530

    added remove outermost layer

---
+++ src/Strings/removeOutermostParenthesis.java
@@ -0,0 +1,28 @@
+package sings;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class removeOutermostParenthesis {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("src/input.txt"));
+        String s = sc.nextLine();
+        int counter = 0;
+        String result = "";
+        for(int i = 0;i<s.length();i++){
+            if(s.charAt(i) == '('){
+                counter ++;
+                if(counter>1) {
+                    result += s.charAt(i);
+                }
+            }else if(s.charAt(i) == ')'){
+                counter --;
+                if(counter>0) {
+                    result += s.charAt(i);
+                }
+            }
+        }
+        System.out.println(result);
+    }
+}
